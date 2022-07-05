6155a805438643c9559640c034664be152951bc1
Alex Bradley <alextbradley.1@gmail.com>
2022-07-05 10:41:51 +0100

    make fig 9 two cols

--- make_figures/make_figure9.m
+++ make_figures/make_figure9.m
@@ -20,7 +20,7 @@ plot_defaults
 label_size = 11;
 ax_fontsize = 10;
 figure(1); clf;
-fig = gcf; fig.Position(3:4) = [400, 800];
+fig = gcf; fig.Position(3:4) = [1085, 800];
 
 % Data locations
 rootdir = '/data/oceans_output/shelf/aleey/mitgcm/APIGi/APIGi_'; %not in git repo
@@ -117,45 +117,115 @@ end %end loop over runs
 end %end gendata loop
 
 %%%%%%%%%%%%%%%%%%%% Plots %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-positions = zeros(10,4);
-positions(:,1) = 0.05;
-positions(:,2) = flip(linspace(0.05, 0.85,10));
-height = diff(positions(:,2));
-height = height(1);
-positions(:,4) = -height;
-positions(:,3) = 0.9;
-colmap = lighter_blue_parula(100,0.2);
 
 
 
-%plot in column
+width = 0.45;
+ncols = 2;
+colgap = 0.07;
+startx = (1 -width*ncols - (ncols-1)*colgap)/2;
+starty = 0.01;
+height = 1/(sz+1);
+positions = zeros(4, ncols, sz);
 for p = 1:sz
-ax(p) = subplot('Position', squeeze(positions(p,:))); hold on
-ax(p).XTick = [];
-ax(p).YTick = [];
+for q = 1:ncols
+positions(:,q,p) = [startx + (q-1)*colgap + (q-1)*width, starty + (p-1)*height, width, height];
+end
+end
+colmap = lighter_blue_parula(100,0.2);
+colbar_ypos = 0.93;
+colbar_xpos = [startx, startx + colgap + width,startx + 2*colgap + 2*width,startx + 3*colgap + 3*width];
+colbar_width = width; cbar_fontsize = 12;
+
+% Column 1
+%
+for p = 1:sz
+ax(1,p) = subplot('Position', squeeze(positions(:,1,sz+1-p)));
+melt = cell2mat(melt_scenarios(p));
+melt = saturate(melt, 120, 0);
+topo = squeeze(cell2mat(topo_scenarios(p)));
+melt(topo == 0) = nan;
+contourf(-Y/1e3,X/1e3,melt, 50, 'linestyle', 'none');
 box on
+hold on
+%add ridge crest and inner cavity
+plot(-[50, 50], [0, max(X/1e3)],'--', 'color', [0.5, 0.5, 0.5])
+plot(-[30, 30], [0, max(X/1e3)],'w--')
+colormap(ax(1,p), colmap)
+xticks([]);
+yticks([]);
+if p == 1
+        a = colorbar;
+        a.Location = 'northoutside';
+        a.Position(1)=colbar_xpos(1);
+        a.Position(2)=colbar_ypos;
+        a.Label.String = 'melt rate (m/yr)';
+        a.FontSize = 10;
+        a.Label.FontSize = cbar_fontsize;
+        a.Label.Interpreter = 'latex';
+end
+
+%add bsf
+stream = cell2mat(bsf_scenarios(p));
+streamsm = smooth2a(stream, 2,2);
+axnew = axes;
+axnew.Position = ax(1,p).Position;
+[C,h] =contour(-Y/1e3,X/1e3, streamsm, [-0.7, -0.5, -0.3, -0.1], 'k');
+%clabel(C,h);
+hold on
+streamsm(1:4,:) = nan; streamsm(end-3:end,:) = nan; streamsm(:,1:60) = nan; streamsm(:,end-4:end) = nan; %remove borders and near Gl where stream is messy
+[C,h] =contour(-Y/1e3,X/1e3, streamsm, [0,0], 'r');
+[C,h] =contour(-Y/1e3,X/1e3, streamsm, [0.05,0.05], 'g');
+xticks([]);
+yticks([]);
+set(axnew, 'color', 'none')
+
+end
+
+
+%
+% Column 2
+%
+
+
+for p = 1:sz
+ax(2,p) = subplot('Position', squeeze(positions(:,2,sz+1-p)));
 TMS = cell2mat(merid_theta_scenarios(p));
-TMS = saturate(TMS, 1.3, -0.5);
-contourf( max(Y) - Y,-Z, TMS',30, 'linestyle', 'none');
+TMS = saturate(TMS, 1.3, -1.2);
+contourf(max(Y) - Y,-Z, TMS',30, 'linestyle', 'none');
 hold on
 topo = cell2mat(topo_scenarios(p));
 plot(max(Y) - Y, topo(merid_idx, :), 'k', 'linewidth', 1)
+%fill the topo
+%fx = [max(Y) - Y,flip(max(Y) - Y)];
+%fy = [topo(merid_idx,:), zeros(size(topo(merid_idx,:)))];
+%fill(fx,fy, 'w')
+
 plot(max(Y) - Y, bathy(merid_idx, :), 'k', 'linewidth', 1)
 xlim([4*1e4, Y(end)])
 ylim([-1100,-300])
-colormap(ax(p), colmap)
-
+colormap(ax(2,p), colmap)
+xticks([]);
+yticks([]);
 if p == 1
         b = colorbar;
         b.Location = 'northoutside';
-        %b.Position(1)=colbar_xpos(2);
-        b.Position(2)=0.95;
-        b.Label.String = '\Theta (\circC)';
-        b.Label.FontSize =10;
+        b.Position(1)=colbar_xpos(2);
+        b.Position(2)=colbar_ypos;
+        b.FontSize = 10;
+        b.Label.String = '$\Theta$ (${}^\circ$C)';
+        b.Label.FontSize = cbar_fontsize;
+        b.Label.Interpreter = 'latex';
+
 end
+
+%add ridge crest and inner cavity
+plot(Y(end)-1e3*[50, 50], [-1100,-300],'--', 'color', [0.5, 0.5, 0.5])
+plot(Y(end)-1e3*[30, 30], [-1100,-300],'w--')
+
 %add salinity contours
 axnew = axes;
-axnew.Position = ax(p).Position;
+axnew.Position = ax(2,p).Position;
 SMS = cell2mat(merid_salt_scenarios(p));
 [C,h] =contour(max(Y) - Y,-Z , SMS', 34.2:0.2:34.6, 'k');
 xticks([]);
@@ -163,13 +233,7 @@ yticks([]);
 set(axnew, 'color', 'none')
 xlim([4*1e4, Y(end)])
 ylim([-1100,-300])
+
 end %end loop over runs
 
 
-%
-% save
-%
-if saveflag
-%saveas(gcf, 'plots/figure9.eps', 'epsc');
-saveas(gcf, 'plots/figure9.png');
-end
