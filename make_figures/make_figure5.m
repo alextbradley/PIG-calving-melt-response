2aafbbdeb82b285cbb4253743791755d74cbee40
Alex Bradley <alextbradley.1@gmail.com>
2022-07-05 10:20:12 +0100

    add two column only option to figure 5

--- make_figures/make_figure5.m
+++ make_figures/make_figure5.m
@@ -11,7 +11,7 @@
 %
 % Flags
 %
-gendata = 1; %specify whether to pass through the generate data loop
+gendata = 0; %specify whether to pass through the generate data loop
 save_flag = 0;
 
 %
@@ -22,7 +22,7 @@ plot_defaults
 label_size = 11;
 ax_fontsize = 10;
 figure(1); clf;
-fig = gcf; fig.Position(3:4) = [1085, 800];
+fig = gcf; fig.Position(3:4) = [1000, 800];
 
 % Data locations
 rootdir = '/data/oceans_output/shelf/aleey/mitgcm/APIGi/APIGi_'; %not in git repo
@@ -206,6 +206,11 @@ end %end gendata loop
 %
 %figure positions
 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%% Option 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Including cross sections of zonal velocity and temperature
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if 0
 width = 0.24;
 ncols = 4;
 colgap = 0.01;
@@ -396,11 +401,144 @@ if save_flag
 
 saveas(gcf, "plots/figure5.png")
 end
+end %end if 0/1 option statement
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%% end option 1 %%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%% Option 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if 1
+width = 0.45;
+ncols = 2;
+colgap = 0.07;
+startx = (1 -width*ncols - (ncols-1)*colgap)/2;
+starty = 0.01;
+height = 1/(sz+1);
+positions = zeros(4, ncols, sz);
+for p = 1:sz
+for q = 1:ncols
+positions(:,q,p) = [startx + (q-1)*colgap + (q-1)*width, starty + (p-1)*height, width, height];
+end
+end
+colmap = lighter_blue_parula(100,0.2);
+colbar_ypos = 0.93;
+colbar_xpos = [startx, startx + colgap + width,startx + 2*colgap + 2*width,startx + 3*colgap + 3*width];
+colbar_width = width; cbar_fontsize = 12;
+
+% Column 1
+%
+for p = 1:sz
+ax(1,p) = subplot('Position', squeeze(positions(:,1,sz+1-p)));
+melt = cell2mat(melt_scenarios(p));
+melt = saturate(melt, 120, 0);
+topo = squeeze(cell2mat(topo_scenarios(p)));
+melt(topo == 0) = nan;
+contourf(-Y/1e3,X/1e3,melt, 50, 'linestyle', 'none');
+box on
+hold on
+%add ridge crest and inner cavity
+plot(-[50, 50], [0, max(X/1e3)],'--', 'color', [0.5, 0.5, 0.5])
+plot(-[30, 30], [0, max(X/1e3)],'w--')
+colormap(ax(1,p), colmap)
+xticks([]);
+yticks([]);
+if p == 1
+	a = colorbar;
+	a.Location = 'northoutside';
+	a.Position(1)=colbar_xpos(1);
+	a.Position(2)=colbar_ypos;
+	a.Label.String = 'melt rate (m/yr)';
+	a.FontSize = 10;
+	a.Label.FontSize = cbar_fontsize;
+	a.Label.Interpreter = 'latex';
+end
+
+%add bsf
+stream = cell2mat(bsf_scenarios(p));
+streamsm = smooth2a(stream, 2,2);
+axnew = axes;
+axnew.Position = ax(1,p).Position;
+min(min(streamsm(idx)))
+[C,h] =contour(-Y/1e3,X/1e3, streamsm, [-0.7, -0.5, -0.3, -0.1], 'k');
+%clabel(C,h);
+hold on
+streamsm(1:4,:) = nan; streamsm(end-3:end,:) = nan; streamsm(:,1:60) = nan; streamsm(:,end-4:end) = nan; %remove borders and near Gl where stream is messy
+[C,h] =contour(-Y/1e3,X/1e3, streamsm, [0,0], 'r');
+[C,h] =contour(-Y/1e3,X/1e3, streamsm, [0.05,0.05], 'g');
+
+
+xticks([]);
+yticks([]);
+set(axnew, 'color', 'none')
+
+end
 
 
 %
-% extra plots: zonal average of Barotropic SF, baroclinic SF, heat content
+% Column 2
 %
 
+for p = 1:sz
+ax(2,p) = subplot('Position', squeeze(positions(:,2,sz+1-p)));
+TMS = cell2mat(merid_theta_scenarios(p));
+TMS = saturate(TMS, 1.3, -1.2);
+contourf(max(Y) - Y,-Z, TMS',30, 'linestyle', 'none');
+hold on
+topo = cell2mat(topo_scenarios(p));
+plot(max(Y) - Y, topo(merid_idx, :), 'k', 'linewidth', 1)
+%fill the topo
+%fx = [max(Y) - Y,flip(max(Y) - Y)];
+%fy = [topo(merid_idx,:), zeros(size(topo(merid_idx,:)))];
+%fill(fx,fy, 'w')
 
+plot(max(Y) - Y, bathy(merid_idx, :), 'k', 'linewidth', 1)
+xlim([4*1e4, Y(end)])
+ylim([-1100,-300])
+colormap(ax(2,p), colmap)
+xticks([]);
+yticks([]);
 
+if p == 1
+	b = colorbar;
+	b.Location = 'northoutside';
+	b.Position(1)=colbar_xpos(2);
+	b.Position(2)=colbar_ypos;
+	b.FontSize = 10;
+	b.Label.String = '$\Theta$ (${}^\circ$C)';
+	b.Label.FontSize = cbar_fontsize;
+	b.Label.Interpreter = 'latex';
+
+end
+
+%add ridge crest and inner cavity
+plot(Y(end)-1e3*[50, 50], [-1100,-300],'--', 'color', [0.5, 0.5, 0.5])
+plot(Y(end)-1e3*[30, 30], [-1100,-300],'w--')
+
+%add salinity contours
+axnew = axes;
+axnew.Position = ax(2,p).Position;
+SMS = cell2mat(merid_salt_scenarios(p));
+[C,h] =contour(max(Y) - Y,-Z , SMS', 34.2:0.2:34.6, 'k');
+xticks([]);
+yticks([]);
+set(axnew, 'color', 'none')
+xlim([4*1e4, Y(end)])
+ylim([-1100,-300])
+
+end %end loop over runs
+
+%
+% Save flag
+%
+set(gcf, 'color', 'w')
+if save_flag
+%saveas(gcf, "plots/figure5", 'epsc')
+
+saveas(gcf, "plots/figure5.png")
+end
+end %end option 2 if 0/1 statement
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%% end option 2 %%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
